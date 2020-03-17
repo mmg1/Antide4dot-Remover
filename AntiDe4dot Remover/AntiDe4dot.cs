dfe33a223bf3438c5362d7175bbb1a3a38f4a03f
Prab <54489997+NotPrab@users.noreply.github.com>
2020-03-17 22:55:17 +0700

    Added
    nothing much about it

---
+++ AntiDe4dot Remover/AntiDe4dot.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using dnlib.DotNet;
+
+namespace AntiDe4dot_Remover
+{
+    class AntiDe4dot
+    {
+        public static void Execute (ModuleDefMD module)
+        {
+            for (int i = 0; i < module.Types.Count; i++)
+            {
+                TypeDef type = module.Types[i];
+                bool hasInterfaces = type.HasInterfaces;
+                if (hasInterfaces)
+                {
+                    for (int j = 0; j < type.Interfaces.Count; j++)
+                    {
+                        bool flag = type.Interfaces[j].Interface.Name.Contains(type.Name) || type.Name.Contains(type.Interfaces[j].Interface.Name);
+                        if (flag)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine($"Detected AntiDe4dot in type {0}", type.Name);
+                            module.Types.RemoveAt(i);
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("[+] Removed AntiDe4dot in type {0}", type.Name);
+                        }
+                    }
+                }
+            }
+        }
+        
+    }
+}
+
---
+++ AntiDe4dot Remover/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using dnlib.DotNet;
+using dnlib.DotNet.Writer;
+
+namespace AntiDe4dot_Remover
+{
+    class Program
+    {
+        public static bool veryVerbose = false;
+        public static string Asmpath;
+        public static ModuleDefMD module;
+        public static Assembly asm;
+
+        static void Main(string[] args)
+        {
+            Console.Title = "AntiDe4dot Cleaner";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            string directory = args[0];
+
+            try
+            {
+                Program.module = ModuleDefMD.Load(directory);
+                Program.asm = Assembly.LoadFrom(directory);
+                Program.Asmpath = directory;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Not a .NET assembly");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+            AssemblyDef assembly = AssemblyDef.Load(directory);
+            try { AntiDe4dot.Execute(module); }
+            catch(Exception e) { Console.WriteLine($"Error while trying to remove Antide4dot ." + e); }
+
+            string text = Path.GetDirectoryName(directory);
+            if (!text.EndsWith("\\"))
+            {
+                text += "\\";
+            }
+            string filename = string.Format("{0}{1}-Removed{2}", text, Path.GetFileNameWithoutExtension(directory), Path.GetExtension(directory));
+            ModuleWriterOptions writerOptions = new ModuleWriterOptions(module);
+            writerOptions.MetaDataOptions.Flags |= MetaDataFlags.PreserveAll;
+            writerOptions.Logger = DummyLogger.NoThrowInstance;
+            NativeModuleWriterOptions NativewriterOptions = new NativeModuleWriterOptions(module);
+            NativewriterOptions.MetaDataOptions.Flags |= MetaDataFlags.PreserveAll;
+            NativewriterOptions.Logger = DummyLogger.NoThrowInstance;
+            if (module.IsILOnly) { module.Write(filename, writerOptions); } else { module.NativeWrite(filename, NativewriterOptions); }
+
+            Console.WriteLine($"File Saved at : {filename}");
+            Console.ReadKey();
+            Environment.Exit(0);
+
+        }
+    }
+}
